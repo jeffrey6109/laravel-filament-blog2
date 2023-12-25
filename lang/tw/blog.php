e1600f5bb98a07c9b4c86209d030d0247c98eab5
Jeffrey_Kwan <striker.jk95@gmail.com>
2023-12-25 18:53:20 +0800

    commit

--- lang/tw/blog.php
+++ lang/tw/blog.php
@@ -7,7 +7,7 @@ return [
     'oldest' => '最老',
     'popular' => '最最受歡迎',
     'search' => '搜索',
-    'recommended_topics' => '推荐类别',
+    'recommended_topics' => '推薦類別',
     'search_placeholder' => '請輸入搜索內容 ...',
 
 ];
